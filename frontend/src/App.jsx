0234d867abaf6581e048afe533544cceaba7d4d1
Realmbird <happychristopher777@gmail.com>
2024-08-17 22:51:50 -0700

    Added App Page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,32 @@
 import { useState } from 'react'
 import ChatWindow from './chat_pages/ChatWindow.jsx'
+import { Link } from "react-router-dom";
 import './App.css'
+
 
 function App() {
   const [login, setLogin] = useState(false);
 
   return (
-    <>
+    <div className='app'>
+      <div className="header">
+        <div className="right">
+          <i className="devicon-github-original colored"></i>
+          
+          <i className="devicon-linkedin-plain colored"></i>
+          <h3>Forest Messanger</h3>
+        </div>
+        <div className="left">
+        <Link className="form-link" to="/login">Sign in</Link>
+        </div>
+        
+      </div>
+      <div className="body">
+        Forest Messenger offers users a seamless communication experience, allowing them to connect with friends, family, or communities in a beautifully designed interface
+      </div>
       
-    </>
+          
+    </div>
   )
 }
 
--- frontend/src/chat_pages/Sidebar.jsx
+++ frontend/src/chat_pages/Sidebar.jsx
@@ -1,12 +1,11 @@
-import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
-import ExploreIcon from '@mui/icons-material/Explore';
-import SvgIcon from '@mui/material/SvgIcon';
 
 function Sidebar () {
     return (
         <div className="sideBar">
-            <AddCircleOutlineIcon className= "icon" />
-            <ExploreIcon className= "icon" />
+            <img src="explore_24dp_5F6368.svg" alt="Navigation" />
+            <img src="groups_24dp_5F6368.svg" alt="ChatRooms" />
+            <img src="add_circle_outline_24dp_5F6368.svg" alt="Add" />
+            <img src="account_circle_24dp_5F6368.svg" alt="Account" />
         </div>
     )
 }
